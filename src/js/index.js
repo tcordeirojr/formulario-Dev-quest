499b4d6c7fda89973045825cb803f05f2a4fe9c3
tcordeirojr <tedcordeirojr@gmail.com>
2024-02-20 03:53:20 -0300

    js atualizado faltando apenas borda vermelha

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,44 @@
-//Botao Enviar//
-const botaoSubmit = document.getElementById("botaoSubmit");
-botaoSubmit.addEventListener('click', function () {
-    
-});
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const telefone = document.querySelector("#telefone");
+const mensagem = document.querySelector("#mensagem");
 
+console.log(nameInput)
 
-//------------------------------------------------------//
-//Validação de Formulario//
-document.getElementById('form').addEventListener('submit', function (event) {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
+    // verificando se o nome esta valido
+    if(nameInput.value === "") {
+        alert("por favor preencha o seu nome");
+        return;
+    }
+    //verificando se o email esta valido 
+    if(email.value === "" || !validarEmail(email.value)){
+        alert("por favor preencha o seu email");
+        return;
+    }
+    // verificando se o telefone esta valido
+    if(telefone.value === ""){
+        alert("por favor preencha o seu telefone");
+        return;
+    }
+    if(mensagem.value === ""){
+        alert("escreva a sua mensagem");
+        return;
+    }
 
-    if (validarFormulario()) {
-        
-        alert('formulario com sucesso');
-        
-    }
-    else{
-        alert("preencha todos os campos obrigatorios");
-    }
+    form.submit()
 });
+    //função para validar email 
+    function validarEmail(email){
+        const emailRegex = new RegExp(
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-z]{2,}$/
+        );
+    if (emailRegex.test(email)){
+        return true;
+    }
+    return false;
+    }
+    
 
-function validarFormulario() {
-    // let nome = document.getElementById('nome');
-    // let email = document.getElementById('email');
-    // let telefone = document.getElementById('telefone');
-    // let mensagem = document.getElementById('mensagem');
-    let forms = document.getElementsByClassName('form');
-
-    // if(nome ==="" || email ==="" || telefone =="" || mensagem ===""){
-        if(forms =! ""){
-        return false;
-    }else(forms =!value)
-    return true; 
-}
-validarFormulario();
